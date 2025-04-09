3c49a927f981f6be4c6a9aa47fe08f86527809a7
EslamAyman <106927185+Eng-Eslamayman@users.noreply.github.com>
2025-04-09 18:19:22 +0200

    add required data annotation to register dto and rename creds to credentials

--- Talabat.BusinessLayer/DTOs/RegisterDto.cs
+++ Talabat.BusinessLayer/DTOs/RegisterDto.cs
@@ -1,9 +1,14 @@
-﻿namespace Talabat.BusinessLayer.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabat.BusinessLayer.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Talabat.BusinessLayer/Services/TokenService.cs
+++ Talabat.BusinessLayer/Services/TokenService.cs
@@ -10,24 +10,17 @@ namespace Talabat.BusinessLayer.Services
 {
     public class TokenService : ITokenService
     {
-        private readonly IConfiguration _config;
-
-        public TokenService(IConfiguration config)
-        {
-            _config = config;
-        }
-
         public string CreateToken(Customer user)
         {
             var claims = new List<Claim> { new Claim("id", user.Id.ToString()) };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecretKey123!"));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var token = new JwtSecurityToken(
                 claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
